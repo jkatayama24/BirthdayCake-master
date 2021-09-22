f8e30036615d26eed422c981a6280e9ea7bf16b4
tashchuk23 <tashchuk23@up.edu>
2021-09-21 17:18:54 -0700

    person C - changed cake color (still need person B's changes)

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -52,6 +52,7 @@ public class CakeView extends SurfaceView {
 
         //Setup our palette
         cakePaint.setColor(0xFFC755B5);  //violet-red
+        cakePaint.setColor(0xFF215F6C);
         cakePaint.setStyle(Paint.Style.FILL);
         frostingPaint.setColor(0xFFFFFACD);  //pale yellow
         frostingPaint.setStyle(Paint.Style.FILL);
