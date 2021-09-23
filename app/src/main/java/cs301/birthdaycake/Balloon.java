50b291323505354cf548ba0786f9595b2c1f1b39
BH <74618590+harringt23@users.noreply.github.com>
2021-09-23 16:40:07 -0700

    implemented the drawing of the balloon inside the balloon class

--- app/src/main/java/cs301/birthdaycake/Balloon.java
+++ app/src/main/java/cs301/birthdaycake/Balloon.java
@@ -1,4 +1,33 @@
 package cs301.birthdaycake;
 
-public class Balloon {
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+public class Balloon
+{
+    /* First add the information the class needs to know */
+    // center x, center y
+    private float cLeft, cTop, cRight, cBottom;
+    // color of balloon
+    private final Paint color;
+
+    // initialize the Balloon constructor
+    // not letting user pick color, just need to know where tapped
+    public Balloon(float x, float y, float r, float b)
+    {
+        // initialize the member variables
+        this.cLeft = x;
+        this.cTop = y;
+        this.cRight = r;
+        this.cBottom = b;
+        color = new Paint();
+        color.setColor(Color.BLUE);
+    }
+    // create public method that anyone who has a reference to the object can access
+    public void draw(Canvas canvas)
+    {
+        // draw the oval based on the parameters passed in (should be center of where touched)
+        canvas.drawOval(this.cLeft, this.cTop, this.cRight, this.cBottom, color);
+    }
 }
